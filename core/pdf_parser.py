c3b7af29629f5177beabc2a3df30e8525ff0fdb0
Sneha Yadav <iyadavsneha18@gmail.com>
2026-06-25 14:40:32 +0530

    added testing resume reader

---
+++ core/pdf_parser.py
@@ -0,0 +1,18 @@
+from pypdf import PdfReader
+
+def extract_text_from_pdf(pdf_path):
+    try:
+        reader = PdfReader(pdf_path)
+
+        text = ""
+
+        for page in reader.pages:
+            page_text = page.extract_text()
+
+            if page_text:
+                text += page_text + "\n"
+
+        return text
+
+    except Exception as e:
+        return f"Error: {e}"
---
+++ test_resume.py
@@ -0,0 +1,7 @@
+from core.pdf_parser import extract_text_from_pdf
+
+print("Testing PDF parser...")
+
+result = extract_text_from_pdf("anything.pdf")
+
+print(result)
